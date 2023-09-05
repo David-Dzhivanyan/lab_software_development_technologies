31e450dcaaab76d36083a1d82540b056f320630a
root <opelik409@gmail.com>
2023-09-06 02:00:04 +0300

    create file

---
+++ lab_1/singleton.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Singleton{
+    void construct(){
+        int i;
+    }
+
+};
+
+
+int main(){
+    cout << "Hello world!" << endl;
+    return 0;
+}
